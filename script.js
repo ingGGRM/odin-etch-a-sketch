b34601de7d479b9a7d684d2eb3bdb00bf7f8ec6b
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-05-30 12:29:57 -0400

    Added a mouse drag/drop blocker to html-body tag. Added js to globally listen mouse clicks. Also, added js to listen mouse clicked over the grid, so it can call a drawer function. Added css to draw black backgroun on selected grid elements.

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ const container = document.getElementById("container");
 const grid_container = document.getElementById("grid-container");
 
 // variables declaration
+let mouseClicked = false;
 
 
+/* MOUSE GLOBAL LISTENER */
+document.addEventListener('mousedown', mouseListener);
+document.addEventListener('mouseup', mouseListener);
+function mouseListener(e) {
+    if(e.type === 'mousedown') mouseClicked = true;
+    if(e.type === 'mouseup') mouseClicked = false;
+    //console.log(mouseClicked);
+}
+
+/* INIT FUNCTION */
 function init() { // function call on full page load
     for(let i = 0; i < 16; i++) { // grid rows creation
         const grid_row = document.createElement('div');
@@ -17,6 +28,14 @@ function init() { // function call on full page load
             const row_element = document.createElement('div');
             row_element.classList.add('grid-elem');
             grid_row.appendChild(row_element);
+            row_element.addEventListener('mousedown', hover); // listen to mouse event to draw
+            row_element.addEventListener('mouseover', hover); // listen to mouse event to draw
         }
     }
+}
+
+/* DRAWER FUNCTION */
+function hover(e) {
+    if(!mouseClicked) return; // if mouse isn´t clicked, return
+    this.classList.add('filled');
 }

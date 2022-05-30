b8684bfe2fa50309273fa2aa0a8d0ea1208f5a49
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-05-30 15:11:56 -0400

    Added html elements with new control such as a color picker. Remove js clicked class becouse drawing method will not use class add/remove. Added js to use constant default variables for the size and the color

--- script.js
+++ script.js
@@ -3,8 +3,20 @@
 // const declarations for elements getters
 const container = document.getElementById("container");
 const grid_container = document.getElementById("grid-container");
+const grid_size_button = document.querySelector(".grid-size");
+const clear_grid_button = document.querySelector(".clear-grid");
+
+const color_picker = document.getElementById("color-picker");
+color_picker.oninput = (e) => color = e.srcElement.value;
 
 // variables declaration
+const DEFAULT_GRID_SIZE = 16;
+const DEFAULT_COLOR = '#000000';
+
+let grid_size = DEFAULT_GRID_SIZE;
+let current_grid_size;
+let color = DEFAULT_COLOR;
+let first_start;
 let mouseClicked = false;
 
 
@@ -19,12 +31,28 @@ function mouseListener(e) {
 
 /* INIT FUNCTION */
 function init() { // function call on full page load
-    for(let i = 0; i < 16; i++) { // grid rows creation
+    current_grid_size = grid_size;
+    setGrid(grid_size);
+
+    /* CONTROL PANEL LISTENER */
+    grid_size_button.addEventListener('click', () => {
+        let grid_size = prompt("Enter a new size for the grid: ");
+        current_grid_size = grid_size;
+        console.log("User selecter the size of: " + grid_size);
+        setGrid(grid_size);
+    });
+    clear_grid_button.addEventListener('click', clearGrid);
+}
+
+function setGrid(size) { // grid generator function
+    /* GRID GENERATION */
+    grid_container.innerHTML = '';
+    for(let i = 0; i < size; i++) { // grid rows creation
         const grid_row = document.createElement('div');
         grid_row.classList.add('rows');
         grid_container.appendChild(grid_row);
 
-        for(let i = 0; i < 16; i++) { // grid column elements for each row element
+        for(let i = 0; i < size; i++) { // grid column elements for each row element
             const row_element = document.createElement('div');
             row_element.classList.add('grid-elem');
             grid_row.appendChild(row_element);
@@ -34,8 +62,11 @@ function init() { // function call on full page load
     }
 }
 
+function clearGrid() {setGrid(current_grid_size);} // grid clearer function
+
 /* DRAWER FUNCTION */
 function hover(e) {
     if(!mouseClicked) return; // if mouse isn´t clicked, return
-    this.classList.add('filled');
+    this.style.background = color;
+    //this.classList.add('filled');
 }

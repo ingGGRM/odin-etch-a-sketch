b70dd4506bd579d551d4d8d2fe458979409eeb17
ing. Genesis G. Rosario <genesis_guarionex_rm@hotmail.com>
2022-05-30 11:40:13 -0400

    Added js code to generate 16x16 grid. Used css flex to arange grid elements

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 /* MAIN JAVASCRIPT FILE */
 
+// const declarations for elements getters
+const container = document.getElementById("container");
+const grid_container = document.getElementById("grid-container");
 
-function init() {
-    
+// variables declaration
+
+
+function init() { // function call on full page load
+    for(let i = 0; i < 16; i++) { // grid rows creation
+        const grid_row = document.createElement('div');
+        grid_row.classList.add('rows');
+        grid_container.appendChild(grid_row);
+
+        for(let i = 0; i < 16; i++) { // grid column elements for each row element
+            const row_element = document.createElement('div');
+            row_element.classList.add('grid-elem');
+            grid_row.appendChild(row_element);
+        }
+    }
 }
